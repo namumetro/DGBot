ae472d94e62d5eae4deb0aba5593fc4b065c740c
나무스튜 <71622706+namumetro@users.noreply.github.com>
2022-03-19 14:18:26 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js")
+const { Client, Intents } = require("discord.js")
+require("dotenv").config()
+const cmdHandler = require("./handler")
+
+const client = new Discord.Client({ intents: [Intents.FLAGS.GUILDS] })
+
+client.once("ready", () => {
+    console.log("Ready!")
+    cmdHandler.set(client)
+})
+
+client.on("messageCreate", async (message) => {
+    if (message.author.bot) return
+
+    if (message.content == "디지야")
+        message.channel.send("나?")
+    else if (message.content.startsWith("디지야 ")
+        cmdHandler.get(client, message)
+})
+
+client.login(process.env.BOT_TOKEN)
